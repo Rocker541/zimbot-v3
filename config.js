3a27405e5d60857db21417de5bcdd1c373fe69e0
Rocker541 <107186342+Rocker541@users.noreply.github.com>
2022-07-26 21:47:12 +0530

    Update config.js
    Signed-off-by: Rocker541 <107186342+Rocker541@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
+global.owner = ['919311270802','']
+global.premium = ['919311270802','']
+global.botname = 'RS BOT'
+global.ownername = 'RAHUL'
 global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
 global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
 global.packname = 'ZIM BOT INC'
